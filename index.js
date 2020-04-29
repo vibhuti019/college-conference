ccb24331225ef5cb59a277cae7fcbef3aca03bd0
Vibhuti Singh <vibhutisingh037@gmail.com>
2020-04-30 02:17:08 +0530

    Updated Sponsorship

--- index.js
+++ index.js
@@ -50,7 +50,80 @@ function changeClass(element){
 
 function sponsorship(){
     elementToChange = document.getElementById("container")
-    elementToChange.innerHTML = " "
+    elementToChange.innerHTML = `
+    <h2 style="text-align: center;">Sponsorship</h2>
+            <p>
+                FITE 2020 is an two days international conference to be held on 15-16 October , 2020 and hosted jointly by Departments of Mechanical Engineering, 
+                Electrical and Electronics Engineering, Electronics and Communication Engineering, Civil Engineering and Master of Business Administration, ABES EC Ghaziabad. 
+                This event will include oral and poster presentation of research papers from various parts of country as well as have international participation from around 
+                10-15 countries of Asia, Europe and U.S.  Keynote talks from experts in academia, industry and prominent dignitaries from Government organization.</br></br>
+
+                FITE 2020 will have phenomenal interest of Scientists and Academicians, Engineers, Industry representatives and Students from all over the globe.</br></br>   
+                We invite you to team with us in promotion of scientific and engineering research by sponsoring the conference. Various opportunities for association 
+                are available as per sponsorship details given below.
+            </p>
+                <ul class="sponsorship-tab-content">
+                    <div class="color-li">
+                        Platinum
+                        <span style="display: inline-block; float: right;">
+                            INR 2-3 lacs
+                        </span>
+                    </div>
+                        <ul>
+                            <li><img src="./icons/check.png" >Recognition on all Press releases. </li>
+                            <li><img src="./icons/check.png" >Company logo and link on the conference website.</li>
+                            <li><img src="./icons/check.png" >Display of banner at institute gate located on NH-24.</li>
+                            <li><img src="./icons/check.png" >Acknowledgement during the inaugural ceremony.</li>
+                            <li><img src="./icons/check.png" >Color advertisement of the sponsor/ product on the front page of the Souvenir.</li>
+                            <li><img src="./icons/check.png" >Company's name and logo printed on the conference proceedings, posters and signage.</li>
+                            <li><img src="./icons/check.png" >Company's brochure and demo CD to be included in Conference kit.</li>
+                            <li><img src="./icons/check.png" >Promotion of sponsoring company on social networking sites of conference.</li>
+                            <li><img src="./icons/check.png" >Sponsoring company can exhibit their products or services on a stall.</li>
+                        </ul>
+                    <div class="color-li">
+                        Gold
+                        <span style="display: inline-block; float: right;">
+                            INR 1-2 lacs
+                        </span>
+                    </div>
+                    <ul>
+                        <li><img src="./icons/check.png" >Company logo and link on the Conference Website.</li>
+                        <li><img src="./icons/check.png" >Acknowledgement during the Inaugural Ceremony. </li>
+                        <li><img src="./icons/check.png" >Colour advertisement of the sponsor/product in Conference Souvenir.</li>
+                        <li><img src="./icons/check.png" >Company's name and logo printed on the Conference Proceedings, Posters, and Signage.</li>
+                        <li><img src="./icons/check.png" >Two complimentary registrations.</li>
+                        <li><img src="./icons/check.png" >One exhibit space. </li>
+                    </ul>
+                    <div class="color-li">
+                        Silver
+                        <span style="display: inline-block; float: right;">
+                            INR 0.5 - 1.0 lacs
+                        </span>
+                    </div>
+                    <ul>
+                        <li><img src="./icons/check.png" >Company logo and link on the Conference Website.</li>
+                        <li><img src="./icons/check.png" >Colour advertisement of the sponsor/ product in the Conference Souvenir. </li>
+                        <li><img src="./icons/check.png" >Company's name and logo printed on the Conference Signage.</li>
+                        <li><img src="./icons/check.png" >Display of Company Banner within the Lunch Area.</li>
+                        <li><img src="./icons/check.png" >One complimentary pass of networking dinner.</li>
+                    </ul>
+                    <div class="color-li">
+                        Bronze
+                        <span style="display: inline-block; float: right;">
+                            Below INR 50000
+                        </span>
+                    </div>
+                        <ul>
+                            <li><img src="./icons/check.png" >Company logo and link on the Conference Website.</li>
+                            <li><img src="./icons/check.png" >Colour advertisement of the sponsor/ product in the Conference Souvenir. </li>
+                            <li><img src="./icons/check.png" >Company's name and logo printed on the Conference Signage.</li>
+                            <li><img src="./icons/check.png" >Display of Company Banner within the Lunch Area.</li>
+                            <li><img src="./icons/check.png" >One complimentary pass of networking dinner.</li>
+                        </ul>
+                                                
+                            
+                </ul>
+    `
 
 
 }
@@ -129,85 +202,85 @@ function home(){
     <div class="topics-covered">
         <ul>
             <li id="machine" onclick="changeClass(this.id)">
-                <a>Machine Design<img id="machine-dropdown" style="display: inline-block; float: right; height: 25px; width: 25px;transform: rotate(0deg);" src="./icons/dropdown-color.png"></a>
+                <a>Machine Design<img id="machine-dropdown" style="display: inline-block; float: right; height: 20px; width: 20px;transform: rotate(0deg);" src="./icons/dropdown-color.png"></a>
                 <ul id="machine-show" class="display-none">
-                    <li><a href="">CAD/CAM/CIM </a> 
-                    <li><a href="">FEM</a> 
-                    <li><a href="">Machine Design and Dynamics,</a> 
-                    <li><a href="">Multi-Body Dynamics</a> 
-                    <li><a href="">Design of Mechanical Systems</a> 
-                    <li><a href="">Fatigue Fracture & Failure Analysis</a> 
-                    <li><a href="">Mechanisms</a> 
-                    <li><a href="">Rotor Dynamics</a> 
-                    <li><a href="">Vibrations and Control</a> 
-                    <li><a href="">Product Design and Development</a> 
+                    <li><a href="">CAD/CAM/CIM </a> </li>
+                    <li><a href="">FEM</a> </li>
+                    <li><a href="">Machine Design and Dynamics,</a> </li> 
+                    <li><a href="">Multi-Body Dynamics</a> </li>
+                    <li><a href="">Design of Mechanical Systems</a>  </li>
+                    <li><a href="">Fatigue Fracture & Failure Analysis</a> </li> 
+                    <li><a href="">Mechanisms</a> </li>
+                    <li><a href="">Rotor Dynamics</a> </li>
+                    <li><a href="">Vibrations and Control</a> </li>
+                    <li><a href="">Product Design and Development</a> </li>
                 </ul>
             </li>
             <li id="tribology" onclick="changeClass(this.id)">
-                <a>Tribology Material<img id="tribology-dropdown" style="display: inline-block; float: right; height: 25px; width: 25px;transform: rotate(0deg);" src="./icons/dropdown-color.png"></a>
+                <a>Tribology Material<img id="tribology-dropdown" style="display: inline-block; float: right; height: 20px; width: 20px;transform: rotate(0deg);" src="./icons/dropdown-color.png"></a>
                 <ul id="tribology-show" class="display-none">
-                    <li><a href="">Bearing & Gear Tribology</a> 
-                    <li><a href="">Lubricants & Additives</a> 
-                    <li><a href="">Wear Mechanisms</a> 
-                    <li><a href="">Surface Treatment & Coatings</a> 
-                    <li><a href="">Tribo-Testing</a> 
-                    <li><a href="">Condition Based Maintenance System</a> 
-                    <li><a href="">Tribology in Design & Manufacturing </a> 
-                    <li><a href="">Tribology in Metalworking</a> 
-                    <li><a href="">Bio Tribology</a> 
-                    <li><a href="">Nano Tribology</a> 
-                    <li><a href="">Nuclear Tribology</a> 
-                    <li><a href="">Space Tribology</a> 
-                    <li><a href="">Automotive Tribology</a> 
-                    <li><a href="">Marine Tribology</a> 
-                    <li><a href="">Tribology in Renewable Energy</a> 
+                    <li><a href="">Bearing & Gear Tribology</a> </li>
+                    <li><a href="">Lubricants & Additives</a> </li>
+                    <li><a href="">Wear Mechanisms</a> </li>
+                    <li><a href="">Surface Treatment & Coatings</a> </li>
+                    <li><a href="">Tribo-Testing</a> </li>
+                    <li><a href="">Condition Based Maintenance System</a> </li>
+                    <li><a href="">Tribology in Design & Manufacturing </a> </li>
+                    <li><a href="">Tribology in Metalworking</a> </li>
+                    <li><a href="">Bio Tribology</a> </li>
+                    <li><a href="">Nano Tribology</a> </li>
+                    <li><a href="">Nuclear Tribology</a> </li>
+                    <li><a href="">Space Tribology</a> </li>
+                    <li><a href="">Automotive Tribology</a> </li>
+                    <li><a href="">Marine Tribology</a> </li>
+                    <li><a href="">Tribology in Renewable Energy</a> </li>
                 </ul>
             </li>
             <li  id="material" onclick="changeClass(this.id)">
-            <a>Material, Manufacturing and Industrial Engineering<img id="material-dropdown" style="display: inline-block; float: right; height: 25px; width: 25px;transform: rotate(0deg);" src="./icons/dropdown-color.png"></a>
+            <a>Material, Manufacturing and Industrial Engineering<img id="material-dropdown" style="display: inline-block; float: right; height: 20px; width: 20px;transform: rotate(0deg);" src="./icons/dropdown-color.png"></a>
                 <ul id="material-show" class="display-none">
-                    <li><a href="">Supply chain</a> 
-                    <li><a href="">Quality and Information management</a> 
-                    <li><a href="">Human factor</a> 
-                    <li><a href="">Biomechanics and Ergonomics</a> 
-                    <li><a href="">Marketing</a> 
-                    <li><a href="">Operation research</a> 
-                    <li><a href="">Layout planning</a> 
-                    <li><a href="">Reverse Engineering </a> 
-                    <li><a href="">CAD/CAM/CIM</a> 
-                    <li><a href="">Reliability and Risk assessment</a> 
-                    <li><a href="">Robotics and Automation</a> 
-                    <li><a href="">Mechatronics</a> 
-                    <li><a href="">Additive and Green Manufacturing</a> 
-                    <li><a href="">Materials Forming & Machining</a> 
-                    <li><a href="">Micro & Nano Machining Technology</a> 
-                    <li><a href="">Laser Processing Technology</a> 
-                    <li><a href="">Bionic Mechanisms and Bio-manufacturing</a> 
-                    <li><a href="">Virtual Manufacturing and Network Manufacturing</a> 
-                    <li><a href="">Sustainable Manufacturing Technologies</a> 
-                    <li><a href="">Production and Operation Management</a> 
+                    <li><a href="">Supply chain</a> </li>
+                    <li><a href="">Quality and Information management</a> </li>
+                    <li><a href="">Human factor</a> </li>
+                    <li><a href="">Biomechanics and Ergonomics</a> </li>
+                    <li><a href="">Marketing</a> </li>
+                    <li><a href="">Operation research</a> </li>
+                    <li><a href="">Layout planning</a> </li>
+                    <li><a href="">Reverse Engineering </a> </li>
+                    <li><a href="">CAD/CAM/CIM</a> </li>
+                    <li><a href="">Reliability and Risk assessment</a> </li>
+                    <li><a href="">Robotics and Automation</a> </li>
+                    <li><a href="">Mechatronics</a> </li>
+                    <li><a href="">Additive and Green Manufacturing</a> </li>
+                    <li><a href="">Materials Forming & Machining</a> </li>
+                    <li><a href="">Micro & Nano Machining Technology</a> </li>
+                    <li><a href="">Laser Processing Technology</a> </li>
+                    <li><a href="">Bionic Mechanisms and Bio-manufacturing</a> </li>
+                    <li><a href="">Virtual Manufacturing and Network Manufacturing</a> </li>
+                    <li><a href="">Sustainable Manufacturing Technologies</a> </li>
+                    <li><a href="">Production and Operation Management</a> </li>
                 </ul>
             </li>
             <li id="thermal" onclick="changeClass(this.id)">
-                <a>Thermal And Energy Engineering<img id="thermal-dropdown" style="display: inline-block; float: right; height: 25px; width: 25px;transform: rotate(0deg);" src="./icons/dropdown-color.png"></a>
+                <a>Thermal And Energy Engineering<img id="thermal-dropdown" style="display: inline-block; float: right; height: 20px; width: 20px;transform: rotate(0deg);" src="./icons/dropdown-color.png"></a>
                 <ul id="thermal-show" class="display-none">
-                    <li><a href="">Computational Fluid dynamics</a> 
-                    <li><a href="">Automotive and Alternate fuels</a> 
-                    <li><a href="">Cryogenics technologies</a> 
-                    <li><a href="">Cold Chain Technologies</a> 
-                    <li><a href="">Heat – Mass Transfer</a> 
-                    <li><a href="">Thermo-fluids</a> 
-                    <li><a href="">Energy Conversion Systems</a> 
-                    <li><a href="">Renewable Energy Technologies</a> 
-                    <li><a href="">Energy and Environment</a> 
-                    <li><a href="">Air Conditioning and Refrigeration</a> 
-                    <li><a href="">Numerical Methods in Fluid Flow and Heat Transfer</a> 
-                    <li><a href="">Transport Phenomena in Porous Media</a> 
-                    <li><a href="">Engine Systems: Design and Simulation</a> 
-                    <li><a href="">Active and Passive Safety</a> 
-                    <li><a href="">Engine Thermodynamics</a> 
-                    <li><a href="">Gas Turbines and Gas Dynamics</a> 
-                    <li><a href="">IC Engine & Engine Technologies</a>
+                    <li><a href="">Computational Fluid dynamics</a> </li>
+                    <li><a href="">Automotive and Alternate fuels</a> </li>
+                    <li><a href="">Cryogenics technologies</a> </li>
+                    <li><a href="">Cold Chain Technologies</a> </li>
+                    <li><a href="">Heat – Mass Transfer</a> </li>
+                    <li><a href="">Thermo-fluids</a> </li>
+                    <li><a href="">Energy Conversion Systems</a> </li>
+                    <li><a href="">Renewable Energy Technologies</a> </li>
+                    <li><a href="">Energy and Environment</a> </li>
+                    <li><a href="">Air Conditioning and Refrigeration</a> </li>
+                    <li><a href="">Numerical Methods in Fluid Flow and Heat Transfer</a> </li>
+                    <li><a href="">Transport Phenomena in Porous Media</a> </li>
+                    <li><a href="">Engine Systems: Design and Simulation</a> </li>
+                    <li><a href="">Active and Passive Safety</a> </li>
+                    <li><a href="">Engine Thermodynamics</a> </li>
+                    <li><a href="">Gas Turbines and Gas Dynamics</a> </li>
+                    <li><a href="">IC Engine & Engine Technologies</a></li>
                 </ul>
             </li>
         </ul>
