fcd90341c36e188306f8088ab1842c133ee111fb
vibhuti019 <vibhuti.singh037@gmail.com>
2020-05-31 05:44:24 +0530

    Registration Page

--- site-javascript.js
+++ site-javascript.js
@@ -48,3 +48,75 @@ function changeClass(element){
     
 
 }
+
+function submitResponse() {
+    var AuthorName = fetchValue("txtName")
+    var AuthorTypeBlock = document.getElementById("ddlAuthorType")
+    var AuthorType = AuthorTypeBlock[AuthorTypeBlock.selectedIndex].label
+    console.log("AuthorType = " + AuthorType)
+    var x = "rdbPages_"   
+    for(i=0; i<=4; i++){
+        var index = x + i
+        var noOfPagesBlock = document.getElementById(index)
+        if (noOfPagesBlock.checked){
+            var noOfPages = i
+            console.log(noOfPages)
+            break
+        }
+    }
+    var Designation = fetchValue("txtDesignation")
+    var Department  = fetchValue("txtDepartment")
+    var Organization = fetchValue("txtOrganization")
+    var Mobile = fetchValue("txtMobile")
+    var Email = fetchValue("txtEmail")
+    var Address = fetchValue("txtAddress")
+    var City = fetchValue("txtCity")
+    var CountryBlock = document.getElementById("ddlCountry")
+    var Country = CountryBlock[CountryBlock.selectedIndex].label
+    console.log(Country)
+    if (sendData){
+        sendResponse(AuthorName,AuthorType,Designation,Department,Organization,Mobile,Email,Address,City,Country)
+        console.log("Response Sent")
+    }
+}
+var displayDialog = true;
+var sendData = true;
+function fetchValue(valueName){
+    var Variable = document.getElementById(valueName)
+    if (Variable.value == ''){
+        if (displayDialog)    
+            alert("Please Enter " + Variable.placeholder)
+        displayDialog = false
+        sendData = false
+    }
+    else {
+        displayDialog = true
+        sendData = true
+        console.log(Variable.value)
+        return Variable.value
+    }
+}
+
+function sendResponse(Name,Type,Designation,Department,Organization,Mobile,Email,Address,City,Country) {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+           if (xmlhttp.status == 200) {
+               alert("The Data Has Been Sent");
+           }
+           else if (xmlhttp.status == 400) {
+              alert("Error Sending Data");
+           }
+           else {
+               alert("Error In Sending Data");
+           }
+        }
+    };
+
+    url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfv1WY_JCBw8UI3Jgls4Qd4q4E7vxcjLxGdMLZwwi5x3k7Sow/formResponse"
+    
+
+    xmlhttp.open("POST", url, true);
+    xmlhttp.send("entry.1040180469="+Name+"&entry.354980592="+Type+"&entry.45339239="+Designation+"&entry.499908739="+Department+"&entry.1813433208="+Organization+"&entry.1815839256="+Mobile+"&entry.984853836="+Email+"&entry.978019621="+Address+"&entry.1180549134="+City+"&entry.447049462="+Country+"&fvv=1&draftResponse=[null,null,\"-6085246042236856651\"] &pageHistory=0&fbzx=-6085246042236856651");
+}
